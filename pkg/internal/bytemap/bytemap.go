8573141fec3b48fb46d82ebecbc00874e87d21a1
hannahhoward <hannah@hannahhoward.net>
2024-10-01 23:59:32 -0700

    refactor(service): cleanup design and add comments
    also adds various fixes, tweeks, and rearchitects to prepare for testing

---
+++ pkg/internal/bytemap/bytemap.go
@@ -0,0 +1,62 @@
+package bytemap
+
+import (
+	"iter"
+	"maps"
+
+	"github.com/storacha-network/go-ucanto/core/iterable"
+)
+
+type byteMap[K ~[]byte, T any] struct {
+	data map[string]T
+}
+
+// ByteMap is a generic for mapping byte array like types to arbitrary data types
+type ByteMap[K ~[]byte, T any] interface {
+	Get(K) T
+	Has(K) bool
+	Set(K, T)
+	Delete(K) bool
+	Size() int
+	Iterator() iter.Seq2[K, T]
+}
+
+// NewByteMap returns a new map of multihash to a data type
+func NewByteMap[K ~[]byte, T any](sizeHint int) ByteMap[K, T] {
+	var stringMap map[string]T
+	if sizeHint == -1 {
+		stringMap = make(map[string]T)
+	} else {
+		stringMap = make(map[string]T, sizeHint)
+	}
+	return &byteMap[K, T]{stringMap}
+}
+
+func (bm *byteMap[K, T]) Get(b K) T {
+	return bm.data[string(b)]
+}
+
+func (bm *byteMap[K, T]) Has(b K) bool {
+	_, ok := bm.data[string(b)]
+	return ok
+}
+
+func (bm *byteMap[K, T]) Set(b K, t T) {
+	bm.data[string(b)] = t
+}
+
+func (bm *byteMap[K, T]) Delete(b K) bool {
+	_, ok := bm.data[string(b)]
+	delete(bm.data, string(b))
+	return ok
+}
+
+func (bm *byteMap[K, T]) Size() int {
+	return len(bm.data)
+}
+
+func (bm *byteMap[K, T]) Iterator() iter.Seq2[K, T] {
+	return iterable.Map2(func(str string, t T) (K, T) {
+		return K(str), t
+	}, maps.All(bm.data))
+}
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -13,7 +13,10 @@ import (
 	"github.com/storacha-network/go-ucanto/core/delegation"
 	"github.com/storacha-network/go-ucanto/did"
 	"github.com/storacha-network/indexing-service/pkg/blobindex"
-	"github.com/storacha-network/indexing-service/pkg/internal/parallelwalk"
+	"github.com/storacha-network/indexing-service/pkg/internal/bytemap"
+	"github.com/storacha-network/indexing-service/pkg/internal/jobwalker"
+	"github.com/storacha-network/indexing-service/pkg/internal/jobwalker/parallelwalk"
+	"github.com/storacha-network/indexing-service/pkg/internal/jobwalker/singlewalk"
 	"github.com/storacha-network/indexing-service/pkg/metadata"
 	"github.com/storacha-network/indexing-service/pkg/service/providerindex"
 	"github.com/storacha-network/indexing-service/pkg/types"
@@ -21,18 +24,21 @@ import (
 
 const defaultConcurrency = 5
 
+// Match narrows parameters for locating providers/claims for a set of multihashes
 type Match struct {
 	Subject []did.DID
 }
 
+// Query is a query for several multihashes
 type Query struct {
 	Hashes []multihash.Multihash
 	Match  Match
 }
 
+// QueryResult describes the found claims and indexes for a given query
 type QueryResult struct {
-	Claims  []delegation.Delegation
-	Indexes []blobindex.ShardedDagIndexView
+	Claims  map[cid.Cid]delegation.Delegation
+	Indexes bytemap.ByteMap[types.EncodedContextID, blobindex.ShardedDagIndexView]
 }
 
 // ProviderIndex is a read/write interface to a local cache of providers that falls back to IPNI
@@ -71,21 +77,165 @@ type BlobIndexLookup interface {
 	Find(ctx context.Context, contextID types.EncodedContextID, fetchURL url.URL, rng *metadata.Range) (blobindex.ShardedDagIndexView, error)
 }
 
+// IndexingService implements read/write logic for indexing data with IPNI, content claims, sharded dag indexes, and a cache layer
 type IndexingService struct {
 	blobIndexLookup BlobIndexLookup
 	claimLookup     ClaimLookup
 	providerIndex   ProviderIndex
+	jobWalker       jobwalker.JobWalker[job, queryState]
 }
 
 type job struct {
-	mh           multihash.Multihash
-	isIndex      bool
-	targetClaims []multicodec.Code
+	mh         multihash.Multihash
+	indexForMh *multihash.Multihash
+	jobType    jobType
 }
 
-var allClaims = []multicodec.Code{metadata.EqualsClaimID, metadata.IndexClaimID, metadata.LocationCommitmentID}
-var justLocation = []multicodec.Code{metadata.LocationCommitmentID}
-var equalsOrLocation = []multicodec.Code{metadata.IndexClaimID, metadata.LocationCommitmentID}
+type jobKey string
+
+func (j job) key() jobKey {
+	k := jobKey(j.mh) + jobKey(j.jobType)
+	if j.indexForMh != nil {
+		k += jobKey(*j.indexForMh)
+	}
+	return k
+}
+
+type jobType string
+
+const standardJobType jobType = "standard"
+const locationJobType jobType = "location"
+const equalsOrLocationJobType jobType = "equals_or_location"
+
+var targetClaims = map[jobType][]multicodec.Code{
+	standardJobType:         {metadata.EqualsClaimID, metadata.IndexClaimID, metadata.LocationCommitmentID},
+	locationJobType:         {metadata.LocationCommitmentID},
+	equalsOrLocationJobType: {metadata.IndexClaimID, metadata.LocationCommitmentID},
+}
+
+type queryState struct {
+	q      *Query
+	qr     *QueryResult
+	visits map[jobKey]struct{}
+}
+
+func (is *IndexingService) jobHandler(mhCtx context.Context, j job, spawn func(job) error, state jobwalker.WrappedState[queryState]) error {
+
+	// check if node has already been visited and ignore if that is the case
+	if !state.CmpSwap(func(qs queryState) bool {
+		_, ok := qs.visits[j.key()]
+		return !ok
+	}, func(qs queryState) queryState {
+		qs.visits[j.key()] = struct{}{}
+		return qs
+	}) {
+		return nil
+	}
+
+	// find provider records related to this multihash
+	results, err := is.providerIndex.Find(mhCtx, providerindex.QueryKey{
+		Hash:         j.mh,
+		Spaces:       state.Access().q.Match.Subject,
+		TargetClaims: targetClaims[j.jobType],
+	})
+	if err != nil {
+		return err
+	}
+	for _, result := range results {
+		// unmarshall metadata for this provider
+		md := metadata.MetadataContext.New()
+		err = md.UnmarshalBinary(result.Metadata)
+		if err != nil {
+			return err
+		}
+		// the provider may list one or more protocols for this CID
+		// in our case, the protocols are just differnt types of content claims
+		for _, code := range md.Protocols() {
+			protocol := md.Get(code)
+			// make sure this is some kind of claim protocol, ignore if not
+			hasClaimCid, ok := protocol.(metadata.HasClaimCid)
+			if !ok {
+				continue
+			}
+			// fetch (from cache or url) the actual content claim
+			claimCid := hasClaimCid.GetClaimCid()
+			url := is.fetchClaimUrl(*result.Provider, claimCid)
+			claim, err := is.claimLookup.LookupClaim(mhCtx, claimCid, url)
+			if err != nil {
+				return err
+			}
+			// add the fetched claim to the results, if we don't already have it
+			state.CmpSwap(
+				func(qs queryState) bool {
+					_, ok := qs.qr.Claims[claimCid]
+					return !ok
+				},
+				func(qs queryState) queryState {
+					qs.qr.Claims[claimCid] = claim
+					return qs
+				})
+
+			// handle each type of protocol
+			switch typedProtocol := protocol.(type) {
+			case *metadata.EqualsClaimMetadata:
+				// for an equals claim, it's published on both the content and equals multihashes
+				// we follow with a query for location claim on the OTHER side of the multihash
+				if string(typedProtocol.Equals.Hash()) != string(j.mh) {
+					// lookup was the content hash, queue the equals hash
+					if err := spawn(job{typedProtocol.Equals.Hash(), nil, locationJobType}); err != nil {
+						return err
+					}
+				} else {
+					// lookup was the equals hash, queue the content hash
+					if err := spawn(job{multihash.Multihash(result.ContextID), nil, locationJobType}); err != nil {
+						return err
+					}
+				}
+			case *metadata.IndexClaimMetadata:
+				// for an index claim, we follow by looking for a location claim for the index, and fetching the index
+				mh := j.mh
+				if err := spawn(job{typedProtocol.Index.Hash(), &mh, equalsOrLocationJobType}); err != nil {
+					return err
+				}
+			case *metadata.LocationCommitmentMetadata:
+				// for a location claim, we just store it, unless its for an index CID, in which case get the full idnex
+				if j.indexForMh != nil {
+					// fetch (from URL or cache) the full index
+					shard := typedProtocol.Shard
+					if shard == nil {
+						c := cid.NewCidV1(cid.Raw, j.mh)
+						shard = &c
+					}
+					url := is.fetchRetrievalUrl(*result.Provider, *shard)
+					index, err := is.blobIndexLookup.Find(mhCtx, result.ContextID, url, typedProtocol.Range)
+					if err != nil {
+						return err
+					}
+					// Add the index to the query results, if we don't already have it
+					state.CmpSwap(
+						func(qs queryState) bool {
+							return !qs.qr.Indexes.Has(result.ContextID)
+						},
+						func(qs queryState) queryState {
+							qs.qr.Indexes.Set(result.ContextID, index)
+							return qs
+						})
+
+					// add location queries for all shards containing the original CID we're seeing an index for
+					shards := index.Shards().Iterator()
+					for shard, index := range shards {
+						if index.Has(*j.indexForMh) {
+							if err := spawn(job{shard, nil, equalsOrLocationJobType}); err != nil {
+								return err
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
 
 // Query returns back relevant content claims for the given query using the following steps
 // 1. Query the IPNIIndex for all matching records
@@ -98,82 +248,17 @@ var equalsOrLocation = []multicodec.Code{metadata.IndexClaimID, metadata.Locatio
 func (is *IndexingService) Query(ctx context.Context, q Query) (QueryResult, error) {
 	initialJobs := make([]job, 0, len(q.Hashes))
 	for _, mh := range q.Hashes {
-		initialJobs = append(initialJobs, job{mh, false, allClaims})
+		initialJobs = append(initialJobs, job{mh, nil, standardJobType})
 	}
-	return parallelwalk.ParallelWalk(ctx, initialJobs, QueryResult{}, func(mhCtx context.Context, j job, spawn func(job) error, stateModifier func(func(QueryResult) QueryResult)) error {
-		results, err := is.providerIndex.Find(mhCtx, providerindex.QueryKey{
-			Hash:         j.mh,
-			Spaces:       q.Match.Subject,
-			TargetClaims: j.targetClaims,
-		})
-		if err != nil {
-			return err
-		}
-		for _, result := range results {
-			md := metadata.MetadataContext.New()
-			err = md.UnmarshalBinary(result.Metadata)
-			if err != nil {
-				return err
-			}
-			for _, code := range md.Protocols() {
-				protocol := md.Get(code)
-				if hasClaimCid, ok := protocol.(metadata.HasClaimCid); ok {
-					claimCid := hasClaimCid.GetClaimCid()
-					url := is.fetchClaimUrl(*result.Provider, claimCid)
-					claim, err := is.claimLookup.LookupClaim(mhCtx, claimCid, url)
-					if err != nil {
-						return err
-					}
-					stateModifier(func(qr QueryResult) QueryResult {
-						qr.Claims = append(qr.Claims, claim)
-						return qr
-					})
-				}
-				switch typedProtocol := protocol.(type) {
-				case *metadata.EqualsClaimMetadata:
-					if string(typedProtocol.Equals.Hash()) != string(j.mh) {
-						// lookup was the content hash, queue the equals hash
-						if err := spawn(job{typedProtocol.Equals.Hash(), false, justLocation}); err != nil {
-							return err
-						}
-					} else {
-						// lookup was the equals hash, queue the content hash
-						if err := spawn(job{multihash.Multihash(result.ContextID), false, justLocation}); err != nil {
-							return err
-						}
-					}
-				case *metadata.IndexClaimMetadata:
-					if err := spawn(job{typedProtocol.Index.Hash(), true, justLocation}); err != nil {
-						return err
-					}
-				case *metadata.LocationCommitmentMetadata:
-					if j.isIndex {
-						shard := typedProtocol.Shard
-						if shard == nil {
-							c := cid.NewCidV1(cid.Raw, j.mh)
-							shard = &c
-						}
-						url := is.fetchRetrievalUrl(*result.Provider, *shard)
-						index, err := is.blobIndexLookup.Find(mhCtx, result.ContextID, url, typedProtocol.Range)
-						if err != nil {
-							return err
-						}
-						stateModifier(func(qr QueryResult) QueryResult {
-							qr.Indexes = append(qr.Indexes, index)
-							return qr
-						})
-						shards := index.Shards().Iterator()
-						for shard := range shards {
-							if err := spawn(job{shard, false, equalsOrLocation}); err != nil {
-								return err
-							}
-						}
-					}
-				}
-			}
-		}
-		return nil
-	}, defaultConcurrency)
+	qs, err := is.jobWalker(ctx, initialJobs, queryState{
+		q: &q,
+		qr: &QueryResult{
+			Claims:  make(map[cid.Cid]delegation.Delegation),
+			Indexes: bytemap.NewByteMap[types.EncodedContextID, blobindex.ShardedDagIndexView](-1),
+		},
+		visits: map[jobKey]struct{}{},
+	}, is.jobHandler)
+	return *qs.qr, err
 }
 
 func (is *IndexingService) fetchClaimUrl(provider peer.AddrInfo, claimCid cid.Cid) url.URL {
@@ -195,6 +280,7 @@ func (is *IndexingService) CacheClaim(ctx context.Context, claim delegation.Dele
 	return errors.New("not implemented")
 }
 
+// PublishClaim caches and publishes a content claim
 // I imagine publish claim to work as follows
 // For all claims except index, just use the publish API on IPNIIndex
 // For index claims, let's assume they fail if a location claim for the index car cid is not already published
@@ -202,4 +288,28 @@ func (is *IndexingService) CacheClaim(ctx context.Context, claim delegation.Dele
 // to assemble all the multihashes in the index advertisement
 func (is *IndexingService) PublishClaim(ctx context.Context, claim delegation.Delegation) error {
 	return errors.New("not implemented")
+}
+
+// Option configures an IndexingService
+type Option func(is *IndexingService)
+
+// WithConcurrency causes the indexing service to process find queries parallel, with the given concurrency
+func WithConcurrency(concurrency int) Option {
+	return func(is *IndexingService) {
+		is.jobWalker = parallelwalk.NewParallelWalk[job, queryState](concurrency)
+	}
+}
+
+// NewIndexingService returns a new indexing service
+func NewIndexingService(blobIndexLookup BlobIndexLookup, claimLookup ClaimLookup, providerIndex ProviderIndex, options ...Option) *IndexingService {
+	is := &IndexingService{
+		blobIndexLookup: blobIndexLookup,
+		claimLookup:     claimLookup,
+		providerIndex:   providerIndex,
+		jobWalker:       singlewalk.SingleWalker[job, queryState],
+	}
+	for _, option := range options {
+		option(is)
+	}
+	return is
 }
