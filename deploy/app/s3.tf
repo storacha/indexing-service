0a28cdb2753dc1c0e5727ff0e37f458c292a58a1
Alan Shaw <alan@storacha.network>
2024-11-13 12:20:10 +0700

    fix: make bucket public

--- deploy/app/s3.tf
+++ deploy/app/s3.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket_public_access_block" "ipni_store_bucket" {
   block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_cors_configuration" "ipni_store_cors" {
