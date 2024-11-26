266ecdd84cf240de8705b256c906fdb6175608d0
ash <alan@storacha.network>
2024-11-26 11:39:05 +0100

    feat: increase log level in staging (#46)
    If the environment is not production then set the log level to "debug".

--- deploy/app/lambda.tf
+++ deploy/app/lambda.tf
@@ -72,6 +72,7 @@ resource "aws_lambda_function" "lambda" {
         PUBLIC_URL = "https://${aws_apigatewayv2_domain_name.custom_domain.domain_name}"
         IPNI_STORE_BUCKET_REGIONAL_DOMAIN = aws_s3_bucket.ipni_store_bucket.bucket_regional_domain_name
         CLAIM_STORE_BUCKET_NAME = aws_s3_bucket.claim_store_bucket.bucket
+        GOLOG_LOG_LEVEL = terraform.workspace == "prod" ? "error" : "debug"
     }
   }
 
