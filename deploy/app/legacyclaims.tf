b3d156cef535ef52b823244422120a3595a85ff9
Vicente Olmedo <vicente@storacha.network>
2024-12-12 13:13:50 +0100

    chore: allow access to legacy buckets and tables

---
+++ deploy/app/legacyclaims.tf
@@ -0,0 +1,82 @@
+variable "legacy_claims_table_name" {
+  description = "The name of the DynamoDB table used by the legacy content-claims service"
+  type = string
+  default = "prod-content-claims-claims-v1"
+}
+
+variable "legacy_claims_bucket_name" {
+  description = "The name of the S3 bucket used by the legacy content-claims service"
+  type = string
+  default = "prod-content-claims-bucket-claimsv1bucketefd46802-1mqz6d8o7xw8"
+}
+
+variable "legacy_block_index_table_name" {
+  description = "The name of the legacy block index DynamoDB table"
+  type = string
+  default = "ep-v1-blocks-cars-position"
+}
+
+data "aws_s3_bucket" "legacy_claims_bucket" {
+  bucket = var.legacy_claims_bucket_name
+}
+
+data "aws_dynamodb_table" "legacy_claims_table" {
+  name = var.legacy_claims_table_name
+}
+
+data "aws_dynamodb_table" "legacy_block_index_table" {
+  name = "${terraform.workspace == "prod" ? "prod" : "staging"}-${var.legacy_block_index_table_name}"
+}
+
+data "aws_iam_policy_document" "lambda_legacy_dynamodb_get_document" {
+  statement {
+    actions = [
+      "dynamodb:GetItem",
+    ]
+    resources = [
+      data.aws_dynamodb_table.legacy_claims_table.arn,
+      data.aws_dynamodb_table.legacy_block_index_table.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_legacy_dynamodb_get" {
+  name        = "${terraform.workspace}-${var.app}-lambda-legacy-dynamodb-get"
+  description = "This policy will be used by the lambda to get data from legacy DynamoDB tables"
+  policy      = data.aws_iam_policy_document.lambda_legacy_dynamodb_get_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_legacy_dynamodb_get" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_legacy_dynamodb_get.arn
+}
+
+data "aws_iam_policy_document" "lambda_legacy_s3_get_document" {
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      "${data.aws_s3_bucket.legacy_claims_bucket.arn}/*"
+    ]
+  }
+  statement {
+    actions = [
+      "s3:ListBucket","s3:GetBucketLocation"
+    ]
+    resources = [
+      data.aws_s3_bucket.legacy_claims_bucket.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_legacy_s3_get" {
+  name        = "${terraform.workspace}-${var.app}-lambda_legacy_s3_get"
+  description = "This policy will be used by the lambda to get objects from legacy S3 buckets"
+  policy      = data.aws_iam_policy_document.lambda_legacy_s3_get_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_legacy_s3_get" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_legacy_s3_get.arn
+}
