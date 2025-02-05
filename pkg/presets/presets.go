8b250a8220b7a4f3b88fb2f8567a414731d11ef3
Alan Shaw <alan@storacha.network>
2025-02-05 15:56:09 +0000

    fix: public key for staging upload service

--- pkg/presets/presets.go
+++ pkg/presets/presets.go
@@ -1,8 +1,8 @@
 package presets
 
 var PrincipalMapping = map[string]string{
-	"did:web:staging.up.storacha.network": "did:key:z6MkqVThfb3PVdgT5yxumxjFFjoQ2vWd26VUQKByPuSB9N91",
+	"did:web:staging.up.storacha.network": "did:key:z6MkhcbEpJpEvNVDd3n5RurquVdqs5dPU16JDU5VZTDtFgnn",
 	"did:web:up.storacha.network":         "did:key:z6MkmbbLigYdv5EuU9tJMDXXUudbySwVNeHNqhQGJs7ALUsF",
-	"did:web:staging.web3.storage":        "did:key:z6MkqVThfb3PVdgT5yxumxjFFjoQ2vWd26VUQKByPuSB9N91",
+	"did:web:staging.web3.storage":        "did:key:z6MkhcbEpJpEvNVDd3n5RurquVdqs5dPU16JDU5VZTDtFgnn",
 	"did:web:web3.storage":                "did:key:z6MkmbbLigYdv5EuU9tJMDXXUudbySwVNeHNqhQGJs7ALUsF",
 }
