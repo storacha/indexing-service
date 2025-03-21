d36b22cfec9813cbc84e24bbacdba80349a021d6
Vicente Olmedo <vicente@storacha.network>
2025-03-21 18:07:25 +0100

    switch to memory-optimized nodes

--- deploy/app/modules/indexer/elasticcache.tf
+++ deploy/app/modules/indexer/elasticcache.tf
@@ -11,9 +11,9 @@ module "caches" {
   app = var.app
   environment = terraform.workspace
 
-  # cache.m7g.large => 2 vCPUs, 6.38 GiB memory, 12.5 Gigabit network, $0.1264/hour
+  # cache.r7g.large => 2 vCPUs, 13.07 GiB memory, 12.5 Gigabit network, $0.1752/hour
   # cache.t4g.micro => 2 vCPUs, 0.5 GiB memory, 5 Gigabit network, $0.0128/hour
-  node_type = terraform.workspace == "prod" ? "cache.m7g.large" : "cache.t4g.micro"
+  node_type = terraform.workspace == "prod" ? "cache.r7g.large" : "cache.t4g.micro"
   
   vpc = {
     id = module.vpc[0].id
