096c2dd39c5cd64d4ba064bec1d384f49b1bfd9c
Hannah Howard <hannah@hannahhoward.net>
2025-03-24 03:40:54 -0600

    fix(infra): rollback multiple deployments (#171)
    # Goals
    
    Rollback the indexer deployment until we figure out a cache propogation
    solution for new data.
    
    # Implemention
    
    This reverts commit f9a90fcc61a3bf01b19aa81151b2ab477e1589e4.

--- deploy/modules/elasticaches/main.tf
+++ deploy/modules/elasticaches/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.86.0"
-    }
-  }
-}
-
 locals {
   caches = toset(["providers", "indexes", "claims"])
 }
