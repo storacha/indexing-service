4d76aa549445daec4bc39e33cfe38549a04b6227
Hannah Howard <hannah@hannahhoward.net>
2025-03-25 12:19:05 -0600

    feat(deploy): add cloudfront distribution for AWS private network (#165)
    # Goals
    
    I now understand the main goal of the cloudfront distribution in front
    of content claims: it is NOT caching content, but requests go
    immediately to an AWS cloudfront edge node, then use Amazon's private
    backbone to get to the regional lambda function.
    
    We see in Honeycomb the effects of NOT having the cloudfront to private
    backbone routing -- trans continental hops over the open internet take
    as long as a second, and you see the indexer perform poorly here vs
    content claims (traces show indexer's processing finish, but cloudflare
    can wait as long as a second for a response)
    
    This PR essentially adds this acceleration to our regional network, and
    is based on
    https://aws.amazon.com/blogs/networking-and-content-delivery/latency-based-routing-leveraging-amazon-cloudfront-for-a-multi-region-active-active-architecture/
    
    accelerated.indexer.storacha.network will pick up requests at edge, then
    use route53 latency based routing to pick a regional indexer, then the
    private AWS backbone to actually transport to the AWS gateway.
    
    # For discussion
    
    - cost - cloudfront is expensive as is API gateway. Right now, this will
    pay for both. Surprisingly, API Gateway is almost as expensive as
    cloudfront. The content claims service gets around API Gateway by using
    lambda function URLs but there's no obvious way to use lambda function
    URLs in multiple regions with Cloudfront. there's still another major
    cost cutting mechanism here: switch off API Gateway + lambda and move to
    Application Load Balancer + Fargate. I continue to feel lambda is a bad
    match for the indexer. BUT that's a tomorrow problem.
    
    ---------
    
    Co-authored-by: Vicente Olmedo <vicente@storacha.network>

---
+++ deploy/app/cloudfront.tf
@@ -0,0 +1,93 @@
+locals {
+  # Only prod gets a CloudFront distribution
+  should_create_cloudfront = terraform.workspace == "prod"
+}
+
+resource "aws_cloudfront_distribution" "indexer" {
+  count = local.should_create_cloudfront ? 1 : 0
+
+  origin {
+    domain_name = "${var.app}.storacha.network"
+    origin_id   = "indexer-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = ""
+
+  default_cache_behavior {
+    target_origin_id       = "indexer-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods = ["GET", "HEAD"]
+
+    # Managed policy AllViewerExceptHostHeader: forward all parameters in viewer requests except for the Host header
+    origin_request_policy_id = "b689b0a8-53d0-40ab-baf2-68738e2966ac"
+
+    # Managed policy CachingDisabled: policy with caching disabled
+    cache_policy_id  = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate.cloudfront_cert[0].arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  aliases = ["accelerated.${var.app}.storacha.network"]
+}
+
+# CloudFront is a global service. Certs must be created in us-east-1, where the core ACM infra lives
+provider "aws" {
+  region = "us-east-1"
+  alias = "acm"
+}
+
+resource "aws_acm_certificate" "cloudfront_cert" {
+  count = local.should_create_cloudfront ? 1 : 0
+
+  provider = aws.acm
+
+  domain_name       = "accelerated.${var.app}.storacha.network"
+  validation_method = "DNS"
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cloudfront_cert_validation" {
+  count = local.should_create_cloudfront ? 1 : 0
+
+  allow_overwrite = true
+  name    = tolist(aws_acm_certificate.cloudfront_cert[0].domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.cloudfront_cert[0].domain_validation_options)[0].resource_record_type
+  zone_id = data.terraform_remote_state.shared.outputs.primary_zone.zone_id
+  records = [tolist(aws_acm_certificate.cloudfront_cert[0].domain_validation_options)[0].resource_record_value]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cloudfront_cert" {
+  count = local.should_create_cloudfront ? 1 : 0
+
+  provider = aws.acm
+
+  certificate_arn = aws_acm_certificate.cloudfront_cert[0].arn
+  validation_record_fqdns = [ aws_route53_record.cloudfront_cert_validation[0].fqdn ]
+}
