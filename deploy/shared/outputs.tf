4284c81326a97ab4bc0df1c8a47b9c0502b15607
Vicente Olmedo <vicente@storacha.network>
2025-06-04 15:27:57 +0200

    don't fail when dev resources are not there

--- deploy/shared/outputs.tf
+++ deploy/shared/outputs.tf
@@ -3,20 +3,20 @@ output "primary_zone" {
 }
 
 output "dev_vpc" {
-  value = {
+  value = length(module.dev_vpc) > 0 ? {
     id = module.dev_vpc[0].id
     cidr_block = module.dev_vpc[0].cidr_block
     subnet_ids = module.dev_vpc[0].subnet_ids
-  }
+  } : null
 }
 
 output "dev_caches" {
-  value = {
+  value = length(module.dev_caches) > 0 ? {
     providers = module.dev_caches[0].providers
     no_providers = module.dev_caches[0].no_providers
     indexes = module.dev_caches[0].indexes
     claims = module.dev_caches[0].claims
     iam_user = module.dev_caches[0].iam_user
     security_group_id = module.dev_caches[0].security_group_id
-  }
+  } : null
 }
