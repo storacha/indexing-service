62344531d720fb68435232f4a3dc8ca388267b72
ash <alan@storacha.network>
2025-06-04 13:37:13 +0100

    fix: add warm storage network principal mappings (#210)

--- pkg/presets/presets.go
+++ pkg/presets/presets.go
@@ -5,8 +5,10 @@ var IPNIFindURL = "https://cid.contact"
 var IPNIAnnounceURLs = []string{"https://cid.contact/announce"}
 
 var PrincipalMapping = map[string]string{
-	"did:web:staging.up.storacha.network": "did:key:z6MkhcbEpJpEvNVDd3n5RurquVdqs5dPU16JDU5VZTDtFgnn",
-	"did:web:up.storacha.network":         "did:key:z6MkqdncRZ1wj8zxCTDUQ8CRT8NQWd63T7mZRvZUX8B7XDFi",
-	"did:web:staging.web3.storage":        "did:key:z6MkhcbEpJpEvNVDd3n5RurquVdqs5dPU16JDU5VZTDtFgnn",
-	"did:web:web3.storage":                "did:key:z6MkqdncRZ1wj8zxCTDUQ8CRT8NQWd63T7mZRvZUX8B7XDFi",
+	"did:web:staging.up.warm.storacha.network": "did:key:z6MkpR58oZpK7L3cdZZciKT25ynGro7RZm6boFouWQ7AzF7v",
+	"did:web:up.warm.storacha.network":         "did:key:z6MkgSttS3n3R56yGX2Eufvbwc58fphomhAsLoBCZpZJzQbr",
+	"did:web:staging.up.storacha.network":      "did:key:z6MkhcbEpJpEvNVDd3n5RurquVdqs5dPU16JDU5VZTDtFgnn",
+	"did:web:up.storacha.network":              "did:key:z6MkqdncRZ1wj8zxCTDUQ8CRT8NQWd63T7mZRvZUX8B7XDFi",
+	"did:web:staging.web3.storage":             "did:key:z6MkhcbEpJpEvNVDd3n5RurquVdqs5dPU16JDU5VZTDtFgnn",
+	"did:web:web3.storage":                     "did:key:z6MkqdncRZ1wj8zxCTDUQ8CRT8NQWd63T7mZRvZUX8B7XDFi",
 }
