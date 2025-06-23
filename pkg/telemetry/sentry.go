bb072b83635bda1085c471c38ee922c09ec67898
Alan Shaw <alan@storacha.network>
2025-06-23 16:43:19 +0100

    test: add tests for sentry logger

--- pkg/telemetry/sentry.go
+++ pkg/telemetry/sentry.go
@@ -8,10 +8,14 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 )
 
+type SentryExceptionCaptureFunc func(err error) *sentry.EventID
+
 // SentryLogger is a logger that sends errors messages to Sentry for error,
 // panic and fatal logs.
 type SentryLogger struct {
-	log *logging.ZapEventLogger
+	system           string
+	log              logging.EventLogger
+	captureException SentryExceptionCaptureFunc
 }
 
 func (s *SentryLogger) Debug(args ...any) {
@@ -23,29 +27,29 @@ func (s *SentryLogger) Debugf(format string, args ...any) {
 }
 
 func (s *SentryLogger) Error(args ...any) {
-	if logging.LogLevel(s.log.Level()) <= logging.LevelError {
-		sentry.CaptureException(fmt.Errorf(formatString(len(args)), args...))
+	if getLevel(s.system) <= logging.LevelError {
+		s.captureException(fmt.Errorf(formatString(len(args)), args...))
 	}
 	s.log.Error(args...)
 }
 
 func (s *SentryLogger) Errorf(format string, args ...any) {
-	if logging.LogLevel(s.log.Level()) <= logging.LevelError {
-		sentry.CaptureException(fmt.Errorf(format, args...))
+	if getLevel(s.system) <= logging.LevelError {
+		s.captureException(fmt.Errorf(format, args...))
 	}
 	s.log.Errorf(format, args...)
 }
 
 func (s *SentryLogger) Fatal(args ...any) {
-	if logging.LogLevel(s.log.Level()) <= logging.LevelFatal {
-		sentry.CaptureException(fmt.Errorf(formatString(len(args)), args...))
+	if getLevel(s.system) <= logging.LevelFatal {
+		s.captureException(fmt.Errorf(formatString(len(args)), args...))
 	}
 	s.log.Fatal(args...)
 }
 
 func (s *SentryLogger) Fatalf(format string, args ...any) {
-	if logging.LogLevel(s.log.Level()) <= logging.LevelFatal {
-		sentry.CaptureException(fmt.Errorf(format, args...))
+	if getLevel(s.system) <= logging.LevelFatal {
+		s.captureException(fmt.Errorf(format, args...))
 	}
 	s.log.Fatalf(format, args...)
 }
@@ -59,15 +63,15 @@ func (s *SentryLogger) Infof(format string, args ...any) {
 }
 
 func (s *SentryLogger) Panic(args ...any) {
-	if logging.LogLevel(s.log.Level()) <= logging.LevelPanic {
-		sentry.CaptureException(fmt.Errorf(formatString(len(args)), args...))
+	if getLevel(s.system) <= logging.LevelPanic {
+		s.captureException(fmt.Errorf(formatString(len(args)), args...))
 	}
 	s.log.Panic(args...)
 }
 
 func (s *SentryLogger) Panicf(format string, args ...any) {
-	if logging.LogLevel(s.log.Level()) <= logging.LevelPanic {
-		sentry.CaptureException(fmt.Errorf(format, args...))
+	if getLevel(s.system) <= logging.LevelPanic {
+		s.captureException(fmt.Errorf(format, args...))
 	}
 	s.log.Panicf(format, args...)
 }
@@ -85,10 +89,24 @@ func (s *SentryLogger) Warnf(format string, args ...any) {
 //
 // Note: you should call [sentry.Init] before using the returned logger.
 func NewSentryLogger(system string) *SentryLogger {
-	return &SentryLogger{log: logging.Logger(system)}
+	return &SentryLogger{
+		system:           system,
+		log:              logging.Logger(system),
+		captureException: sentry.CaptureException,
+	}
 }
 
 // formatString gets a format string for the specified number of arguments.
 func formatString(n int) string {
 	return strings.Repeat(" %+v", n)[1:]
+}
+
+// getLevel gets the configured log level for the passed subsystem.
+func getLevel(system string) logging.LogLevel {
+	cfg := logging.GetConfig()
+	lvl, ok := cfg.SubsystemLevels[system]
+	if !ok {
+		return cfg.Level
+	}
+	return lvl
 }
