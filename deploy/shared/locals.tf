9ad2270b17575b4a9697304de71eb8e5cbff536a
Vicente Olmedo <vicente@storacha.network>
2025-06-23 13:07:00 +0200

    delete deployment-related files

--- deploy/shared/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  is_production = terraform.workspace == "prod" || terraform.workspace == "warm-prod"
-  is_staging = terraform.workspace == "staging" || terraform.workspace == "warm-staging"
-}
