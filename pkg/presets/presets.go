ef8bb0003eebc3cb86d016bef8c86231ae9e22d2
Vicente Olmedo <vicente@storacha.network>
2025-11-06 11:31:05 +0100

    update presets

--- pkg/presets/presets.go
+++ pkg/presets/presets.go
@@ -6,10 +6,11 @@ var IPNIAnnounceURLs = []string{"https://cid.contact/announce"}
 
 var PrincipalMapping = map[string]string{
 	"did:web:staging.up.warm.storacha.network":        "did:key:z6MkpR58oZpK7L3cdZZciKT25ynGro7RZm6boFouWQ7AzF7v",
-	"did:web:up.warm.storacha.network":                "did:key:z6MkgSttS3n3R56yGX2Eufvbwc58fphomhAsLoBCZpZJzQbr",
+	"did:web:up.forge.storacha.network":               "did:key:z6MkgSttS3n3R56yGX2Eufvbwc58fphomhAsLoBCZpZJzQbr",
 	"did:web:staging.up.storacha.network":             "did:key:z6MkhcbEpJpEvNVDd3n5RurquVdqs5dPU16JDU5VZTDtFgnn",
 	"did:web:up.storacha.network":                     "did:key:z6MkqdncRZ1wj8zxCTDUQ8CRT8NQWd63T7mZRvZUX8B7XDFi",
 	"did:web:staging.web3.storage":                    "did:key:z6MkhcbEpJpEvNVDd3n5RurquVdqs5dPU16JDU5VZTDtFgnn",
 	"did:web:web3.storage":                            "did:key:z6MkqdncRZ1wj8zxCTDUQ8CRT8NQWd63T7mZRvZUX8B7XDFi",
 	"did:web:staging.registrar.warm.storacha.network": "did:key:z6MkuQ8PfSMrzXCwZkbQv662nZC4FGGm1aucbH256HXXZyxo",
+	"did:web:registrar.forge.storacha.network":        "did:key:z6MkkfWep96Dphp35s9VqSCD7h7G4R9R1QCR3K9TxpbSRrKf",
 }
